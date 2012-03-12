9239b322eb05a7522d6c3ca1c1083c404ab7b825
zhuanqs <p4ssw0rd@163.com>
2012-03-12 23:29:27 +0800

    hello di sample

---
+++ DI-Sample/HelloDI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HelloDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMessageWriter writer = new ConsoleMessageWriter();
+            var salutation = new Salutation(writer);
+            salutation.Exclaim();
+        }
+    }
+
+    public class Salutation
+    {
+        private readonly IMessageWriter _writer;
+
+        public Salutation(IMessageWriter writer)
+        {
+            if (writer == null)
+            {
+                throw new ArgumentNullException("writer");
+            }
+            _writer = writer;
+        }
+
+        public void Exclaim()
+        {
+            _writer.Write("Hello, DI!");
+        }
+    }
+
+    public class ConsoleMessageWriter : IMessageWriter
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public interface IMessageWriter
+    {
+        void Write(string message);
+    }
+}
